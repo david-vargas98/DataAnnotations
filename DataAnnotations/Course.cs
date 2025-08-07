5763c5de5a1539be4e78d6a2f801c9a455e691c2
david-vargas98 <edgar981125@live.com.mx>
2025-08-07 12:48:39 -0600

    refactor: Overriding conventions.
    Making use of overriding to change conventions by using data annotations.

--- DataAnnotations/Course.cs
+++ DataAnnotations/Course.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DataAnnotations
 {
@@ -10,16 +12,22 @@ namespace DataAnnotations
         }
 
         public int Id { get; set; }
-
+        
+        [Required]
+        [MaxLength(255)]
         public string Name { get; set; }
 
+        [Required]
+        [MaxLength(2000)]
         public string Description { get; set; }
 
         public int Level { get; set; }
 
         public float FullPrice { get; set; }
 
-        public virtual Author Author { get; set; }
+        [ForeignKey("Author")] // name of the navigation property to make AuthorId to be linked to Author property
+        public int AuthorId { get; set; }
+        public virtual Author Author { get; set; } // navigation property
         public virtual ICollection<Tag> Tags { get; set; }
     }
 }
